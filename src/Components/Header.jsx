5c79837cd28618a39a3a6336632ef79018431a2a
Harsh Raut <harshraut22@gmail.com>
2024-05-01 00:09:05 +0530

    UI changes for header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import "../Styles/HeaderStyles.css";
 
 const Header = () => {
   return (
-    <header>
+    <header className="headr-cntr">
       Header
-      <img src="../assets/bell.svg" alt="" />
-      <img src="../assets/info.svg" alt="" />
-      <img src="../assets/plus.svg" alt="" />
-      <img src="../assets/profile.svg" alt="" />
+      <button className="header-btn"><img src="../assets/bell.svg" alt="" className="header-bell"/></button>
+      <button className="header-btn"><img src="../assets/info.svg" alt="" className="header-info"/></button>
+      <button className="header-btn"><img src="../assets/plus.svg" alt="" className="header-plus"/></button>
+      <button className="header-btn"><img src="../assets/profile.svg" alt="" className="header-profile"/></button>
     </header>
   );
 };
