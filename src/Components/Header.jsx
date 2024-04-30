f7ebb7d311231ee328c38e44a5d31ed591c8f13b
Harsh Raut <harshraut22@gmail.com>
2024-04-30 20:19:54 +0530

    Initialized repo with header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      Header
+      <img src="../assets/bell.svg" alt="" />
+      <img src="../assets/info.svg" alt="" />
+      <img src="../assets/plus.svg" alt="" />
+      <img src="../assets/profile.svg" alt="" />
+    </header>
+  );
+};
+
+export default Header;
