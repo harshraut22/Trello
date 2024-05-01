4ddadeee0ffd741d1ddcb4942c7e9f30148eb39c
Harsh Raut <harshraut22@gmail.com>
2024-05-01 15:07:54 +0530

    UI update for sidebar and header

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Header from './Components/Header'
+import SideBar from './Components/SideBar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header></Header>
-  </React.StrictMode>,
+    <SideBar></SideBar>
+  </React.StrictMode>
 )
