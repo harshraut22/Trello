91baba5d932010cb40278a20532746d9f931b615
Harsh Raut <harshraut22@gmail.com>
2024-05-02 12:30:23 +0530

    UI update for Cards

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const Card = ({ card }) => {
+  const cardStyles = {
+    backgroundColor : "white",
+    padding : "5px",
+    margin: "2vh"
+  }
   return (
-    <div className="card">
+    <div className="card" style={cardStyles}>
       <h3 className="card-title">{card.title}</h3>
       <p className="card-label">{card.label}</p>
       <p className="card-description">{card.description}</p>
